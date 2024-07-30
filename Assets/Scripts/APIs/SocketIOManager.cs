9d6938d8cba51eff9abca33abc8a7feef514bc5a
EkanshBatra <>
2024-07-30 15:06:44 +0530

    Gamble function update and Balance Dotween Update

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -318,6 +318,8 @@ public class SocketIOManager : MonoBehaviour
                 {
                     Debug.Log(jsonObject);
                     myMessage = myData.message;
+                    playerdata.Balance = myData.message.Balance;
+                    playerdata.currentWining = myData.message.currentWining;
                     isResultdone = true;
                     break;
                 }
@@ -583,7 +585,8 @@ public class Message
     public LowCard lowCard { get; set; }
     public List<ExCard> exCards { get; set; }
     public bool playerWon { get; set; }
-    public int winningAmount { get; set; }
+    public double Balance { get; set; }
+    public double currentWining { get; set; }
     public double maxGambleBet { get; set; }
 }
 
